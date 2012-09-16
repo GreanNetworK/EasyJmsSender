b532219829ed9af0ac5cb28737fbab344612fd3d
Grean <Grean@DELL.mshome.net>
2012-09-16 23:36:34 +0700

    initial commit

---
+++ EasyJmsSender/src/cc/co/greannetwork/EasyJmsSender.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc.co.greannetwork;
+
+/**
+ *
+ * @author Grean
+ */
+public class EasyJmsSender {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
